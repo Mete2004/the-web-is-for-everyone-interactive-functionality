8d0b5798823df86064afc2126c858532a3cda4a8
Mete2004 <metekaansariguney58@gmail.com>
2026-04-01 13:29:06 +0200

    styling

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
-console.log("JS werkt");
-
+// enhancement
 const menu = document.getElementById('menu');
 const mobileNav = document.getElementById('mobileNav');
 
@@ -8,35 +7,35 @@ menu.addEventListener('click', () => {
 });
 
 
-const form = document.querySelector(".reactie-form");
+// const form = document.querySelector(".reactie-form");
 
-if (form) {
-    form.addEventListener("submit", async function (event) {
-        event.preventDefault();
+// if (form) {
+//     form.addEventListener("submit", async function (event) {
+//         event.preventDefault();
 
-        const formData = new FormData(form);
+//         const formData = new FormData(form);
 
-        try {
-            await fetch("/reacties", {
-                method: "POST",
-                body: formData
-            });
+//         try {
+//             await fetch("/reacties", {
+//                 method: "POST",
+//                 body: formData
+//             });
 
-            // check of er al een message is
-            const existingMessage = document.querySelector(".success-message-outer");
+//             // check of er al een message is
+//             const existingMessage = document.querySelector(".success-message-outer");
 
-            if (!existingMessage) {
-                const message = document.createElement("div");
-                message.classList.add("success-message-outer");
-                message.innerHTML = `<p class="success-message">Je reactie is geplaatst!</p>`;
-                form.parentElement.prepend(message);
-            }
+//             if (!existingMessage) {
+//                 const message = document.createElement("div");
+//                 message.classList.add("success-message-outer");
+//                 message.innerHTML = `<p class="success-message">Je reactie is geplaatst!</p>`;
+//                 form.parentElement.prepend(message);
+//             }
 
-            form.reset();
+//             form.reset();
 
-        } catch (error) {
-            alert("Er ging iets mis");
-        }
-    });
-}
+//         } catch (error) {
+//             alert("Er ging iets mis");
+//         }
+//     });
+// }
 
